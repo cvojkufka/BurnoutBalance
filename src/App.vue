5edfcd117b0eeb5a375894300df8b5658df96ede
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2026-03-17 16:11:16 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <h1>Burnout Balance</h1>
+    <p>Take a moment to check in.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.app {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
